4ad301a8c5adef5b4cf2e4d1028b6dc133278bec
Tuan Do <tuandmcn@hotmail.com>
2025-05-25 11:16:43 +0700

    Update code

--- bookservice/src/main/java/com/ltfullstack/bookservice/command/model/BookRequestModel.java
+++ bookservice/src/main/java/com/ltfullstack/bookservice/command/model/BookRequestModel.java
@@ -1,5 +1,8 @@
 package com.ltfullstack.bookservice.command.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +14,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class BookRequestModel {
     private String id;
-
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 2, max = 30, message = "Name must be between 2 and 30 character")
     private String name;
+    @NotBlank(message = "Author is mandatory")
+
     private String author;
     private Boolean isReady;
 }
--- commonservice/src/main/java/com/ltfullstack/commonservice/advice/ExceptionAdvice.java
+++ commonservice/src/main/java/com/ltfullstack/commonservice/advice/ExceptionAdvice.java
@@ -4,14 +4,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @ControllerAdvice
 @Slf4j
 public class ExceptionAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String,String>> handelValidationException(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((org.springframework.validation.FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler
     public ResponseEntity<ErrorMessage> handleException(Exception ex){
         log.error("Unknown internal server error: "+ex.getMessage());
