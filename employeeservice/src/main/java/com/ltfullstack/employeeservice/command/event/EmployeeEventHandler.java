984ce3efdf973ffcff4b6b12e37049c5fd15da1c
Tuan Do <tuandmcn@hotmail.com>
2025-05-27 21:30:57 +0700

    Update

--- employeeservice/src/main/java/com/ltfullstack/employeeservice/command/event/EmployeeEventHandler.java
+++ employeeservice/src/main/java/com/ltfullstack/employeeservice/command/event/EmployeeEventHandler.java
@@ -3,6 +3,8 @@ package com.ltfullstack.employeeservice.command.event;
 import com.ltfullstack.employeeservice.command.data.Employee;
 import com.ltfullstack.employeeservice.command.data.EmployeeRepository;
 import jakarta.ws.rs.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.eventhandling.DisallowReplay;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class EmployeeEventHandler {
 
@@ -36,11 +39,14 @@ public class EmployeeEventHandler {
     }
 
     @EventHandler
-    public void on(EmployeeDeletedEvent event) throws Exception{
-        employeeRepository.findById(event.getId()).orElseThrow(() -> new Exception("Employee not found"));
-        employeeRepository.deleteById(event.getId());
+    @DisallowReplay
+    public void on(EmployeeDeletedEvent event) {
+        try {
+            employeeRepository.findById(event.getId()).orElseThrow(() -> new Exception("Employee not found"));
+            employeeRepository.deleteById(event.getId());
+
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
     }
-
-
-
 }
