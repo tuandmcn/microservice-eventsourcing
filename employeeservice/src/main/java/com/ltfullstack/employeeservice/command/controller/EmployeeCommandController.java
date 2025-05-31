a9e7709f77bd40a67b78b33d9ec37303f3dd4201
Tuan Do <tuandmcn@hotmail.com>
2025-05-31 23:17:49 +0700

    Cap nhat

--- employeeservice/src/main/java/com/ltfullstack/employeeservice/command/controller/EmployeeCommandController.java
+++ employeeservice/src/main/java/com/ltfullstack/employeeservice/command/controller/EmployeeCommandController.java
@@ -6,6 +6,7 @@ import com.ltfullstack.employeeservice.command.command.UpdateEmployeeCommand;
 import com.ltfullstack.employeeservice.command.data.Employee;
 import com.ltfullstack.employeeservice.command.model.CreateEmployeeModel;
 import com.ltfullstack.employeeservice.command.model.UpdateEmployeeModel;
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.Valid;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,7 @@ public class EmployeeCommandController {
         UpdateEmployeeCommand command= new UpdateEmployeeCommand(employeeId,model.getFirstName(),model.getLastName(),model.getKin(),model.getIsDisciplined());
         return commandGateway.sendAndWait(command);
     }
+    @Hidden
     @DeleteMapping("/{employeeId}")
     public String deleteEmployee(@PathVariable String employeeId) {
         DeleteEmployeeCommand command= new DeleteEmployeeCommand(employeeId);
--- employeeservice/src/main/java/com/ltfullstack/employeeservice/query/controller/EmployeeQueryController.java
+++ employeeservice/src/main/java/com/ltfullstack/employeeservice/query/controller/EmployeeQueryController.java
@@ -4,6 +4,10 @@ import com.ltfullstack.employeeservice.command.data.Employee;
 import com.ltfullstack.employeeservice.query.model.EmployeeResponseModel;
 import com.ltfullstack.employeeservice.query.queries.GetAllEmployeeQuery;
 import com.ltfullstack.employeeservice.query.queries.GetDetailEmployeeQuery;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.axonframework.messaging.responsetypes.ResponseType;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
@@ -13,10 +17,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/employee")
+@RequestMapping("/api/v1/employees")
+@Tag(name="Employee Query")
+@Hidden
 public class EmployeeQueryController {
     @Autowired
     private QueryGateway queryGateway;
+
+    @Operation(
+            summary = "Get list employee",
+            description = "Get endpoint for employee with filter",
+            responses = {
+                    @ApiResponse(
+                            description = "Seccess",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized / Invalid Token"
+                    )
+            }
+    )
 
     @GetMapping
     public List<EmployeeResponseModel> getAllEmployees(@RequestParam(required = false, defaultValue = "false") Boolean isDisciplined) {
