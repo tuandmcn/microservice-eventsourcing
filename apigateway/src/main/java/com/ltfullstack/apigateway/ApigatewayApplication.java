e1b62ba798e5997fa43d050216a6af72c7d85c74
Tuan Do <tuandmcn@hotmail.com>
2025-06-03 22:36:49 +0700

    Code

--- apigateway/src/main/java/com/ltfullstack/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/ltfullstack/apigateway/ApigatewayApplication.java
@@ -5,18 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan({"com.ltfullstack.apigateway","com.ltfullstack.commonservice"})
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
-    @Bean
-    public KeyResolver keyResolver() {
-        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
-    }
+	@Bean
+	public KeyResolver keyResolver(){
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+	}
 }
